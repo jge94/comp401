fe9ab340172890b3d2b9f0b346ed24b6780fce1f
jge <ge_jinnan@wheatoncollege.edu>
2015-04-07 12:59:07 -0400

    psedoCdoe

---
+++ project03/pseudo_code.cpp
@@ -0,0 +1,22 @@
+function Dijkstra(Graph, source):
+		dist[source] ← 0                      // Initialization
+		for each vertex v in Graph:           
+			if v ≠ source
+				dist[v] ← infinity            // Unknown distance from source to v
+                prev[v] ← undefined           // Predecessor of v
+           end if
+           Q.add_with_priority(v, dist[v])
+       end for 
+ 
+       while Q is not empty:               // The main loop
+           u ← Q.extract_min()            // Remove and return best vertex
+           for each neighbor v of u:
+               alt = dist[u] + length(u, v) 
+               if alt < dist[v]
+                   dist[v] ← alt
+                   prev[v] ← u
+                   Q.decrease_priority(v, alt)
+               end if
+           end for
+     end while
+    return prev[]
